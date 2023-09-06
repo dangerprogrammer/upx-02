850c668fd9ec920dbf05c9c77ed1a242a9ba06d5
Danger <104269678+dangerprogrammer@users.noreply.github.com>
2023-09-06 16:05:34 -0300

    .

--- components/context/systemCategories.js
+++ components/context/systemCategories.js
@@ -1,10 +1,10 @@
 import Image from "next/image";
 
 const systemCategories = [
-    { name: 'Eletrônico estragado', ImageCategorie: () => <Image src="@/public/imgs/broken-phone.png"/>, id: 'damaged-phones1' },
-    { name: 'Recondicionar', ImageCategorie: () => <Image src="@/public/imgs/broken-phone.png"/>, id: 'damaged-phones2' },
-    { name: 'Peças e acessórios', ImageCategorie: () => <Image src="@/public/imgs/broken-phone.png"/>, id: 'damaged-phones3' },
-    { name: 'Outros', ImageCategorie: () => <Image src="@/public/imgs/broken-phone.png"/>, id: 'damaged-phones4' },
+    { name: 'Eletrônico estragado', ImageCategorie: () => <Image alt='' src="@/public/imgs/broken-phone.png"/>, id: 'damaged-phones1' },
+    { name: 'Recondicionar', ImageCategorie: () => <Image alt='' src="@/public/imgs/broken-phone.png"/>, id: 'damaged-phones2' },
+    { name: 'Peças e acessórios', ImageCategorie: () => <Image alt='' src="@/public/imgs/broken-phone.png"/>, id: 'damaged-phones3' },
+    { name: 'Outros', ImageCategorie: () => <Image alt='' src="@/public/imgs/broken-phone.png"/>, id: 'damaged-phones4' },
 ];
 
 export default systemCategories;
