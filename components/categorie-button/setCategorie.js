08a4208192893ed836ad49e113bb58fd4002dc91
Danger <104269678+dangerprogrammer@users.noreply.github.com>
2023-09-14 17:04:04 -0300

    .

--- components/categorie-button/setCategorie.js
+++ components/categorie-button/setCategorie.js
@@ -4,8 +4,9 @@ function setCategorie({ id, classList }, { userCategories, setUserCategories }) 
     const cloneCategories = [...userCategories],
         hasActived = classList.toggle(activedCategorie);
 
-    if (hasActived) cloneCategories.push(id);
-    else do cloneCategories.splice(cloneCategories.indexOf(id), 1);
+    if (hasActived) {
+        if (cloneCategories.indexOf(id) === -1) cloneCategories.push(id);
+    } else do cloneCategories.splice(cloneCategories.indexOf(id), 1);
     while (cloneCategories.indexOf(id) !== -1);
 
     localStorage.setItem('user-categories', JSON.stringify(cloneCategories));
