21d9bf9e6620673fcb4c7219aeba6f15a54f243e
Danger <104269678+dangerprogrammer@users.noreply.github.com>
2023-09-14 16:21:11 -0300

    .

--- components/categorie-button/setCategorie.js
+++ components/categorie-button/setCategorie.js
@@ -3,14 +3,14 @@ import { activedCategorie } from './CategorieButton.module.scss';
 import { ContextApp } from '../context/ContextApp';
 
 function setCategorie({ id }) {
-    const { userCategories, setUserCategories } = useContext(ContextApp), cloneCategories = [...userCategories];
+    const { userCategories, setUserCategories } = useContext(ContextApp)//, cloneCategories = [...userCategories];
     const hasActived = elem.classList.toggle(activedCategorie);
 
-    if (hasActived) cloneCategories.push(id);
-    else cloneCategories.splice(cloneCategories.indexOf(id), 1);
+    //if (hasActived) cloneCategories.push(id);
+    //else cloneCategories.splice(cloneCategories.indexOf(id), 1);
 
-    console.log(cloneCategories);
-    setUserCategories(cloneCategories);
+    console.log(userCategories);
+    // setUserCategories(cloneCategories);
 };
 
 export default setCategorie;
