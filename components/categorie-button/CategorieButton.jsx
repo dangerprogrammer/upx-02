5c9b47cb49b4a203fcca1257962b9cbd3f573eb4
Danger <104269678+dangerprogrammer@users.noreply.github.com>
2023-09-14 16:31:03 -0300

    .

--- components/categorie-button/CategorieButton.jsx
+++ components/categorie-button/CategorieButton.jsx
@@ -1,14 +1,8 @@
-// import { useContext } from 'react';
-import { categorieButton, activedCategorie } from './CategorieButton.module.scss';
+import { categorieButton } from './CategorieButton.module.scss';
 import setCategorie from './setCategorie';
-// import { ContextApp } from '../context/ContextApp';
 
 function CategorieButton({ name, ImageCategorie, id }) {
-    /*const { userCategories } = useContext(ContextApp), findedCategorie = userCategories.find(categorie => categorie == id),
-        text = findedCategorie ? ` ${findedCategorie}` : '';*/
-    const text = '';
-
-    return <li className={`${categorieButton}${text}`} id={id} onClick={({ target }) => setCategorie(target)}>
+    return <li className={categorieButton} id={id} onClick={({ target }) => setCategorie(target)}>
         <div>
             <ImageCategorie/>
         </div>
--- components/categorie-button/setCategorie.js
+++ components/categorie-button/setCategorie.js
@@ -1,15 +1,17 @@
-import { useContext } from 'react';
+// import { useContext } from 'react';
 import { activedCategorie } from './CategorieButton.module.scss';
-import { ContextApp } from '../context/ContextApp';
+// import { ContextApp } from '../context/ContextApp';
 
 function setCategorie({ id }) {
-    const { userCategories, setUserCategories } = useContext(ContextApp)//, cloneCategories = [...userCategories];
+    // const { userCategories, setUserCategories } = useContext(ContextApp), cloneCategories = [...userCategories];
     const hasActived = elem.classList.toggle(activedCategorie);
 
-    //if (hasActived) cloneCategories.push(id);
-    //else cloneCategories.splice(cloneCategories.indexOf(id), 1);
+    console.log(hasActived);
 
-    console.log(userCategories);
+    // if (hasActived) cloneCategories.push(id);
+    // else cloneCategories.splice(cloneCategories.indexOf(id), 1);
+
+    // console.log(userCategories);
     // setUserCategories(cloneCategories);
 };
 
