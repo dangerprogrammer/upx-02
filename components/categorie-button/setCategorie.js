0e37eab9d2a48fafcddf2d36aa5c791f0b6e25e9
Danger <104269678+dangerprogrammer@users.noreply.github.com>
2023-09-15 00:37:30 -0300

    .

--- components/categorie-button/setCategorie.js
+++ components/categorie-button/setCategorie.js
@@ -3,7 +3,7 @@ import { activedCategorie } from './CategorieButton.module.scss';
 function setCategorie({ id, classList }, { userCategories, setUserCategories }) {
     const cloneCategories = [...userCategories], hasActived = classList[classList.contains(activedCategorie) ? 'remove' : 'add'](activedCategorie);
 
-    console.log(hasActived);
+    console.log(classList.contains(activedCategorie));
     if (hasActived) {
         if (cloneCategories.indexOf(id) === -1) cloneCategories.push(id);
     } else cloneCategories.splice(cloneCategories.indexOf(id), 1);
