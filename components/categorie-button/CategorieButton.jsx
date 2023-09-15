cc7cecaded3faf756cc7d721910516638bdf0edb
Danger <104269678+dangerprogrammer@users.noreply.github.com>
2023-09-15 02:02:24 -0300

    .

--- components/categorie-button/CategorieButton.jsx
+++ components/categorie-button/CategorieButton.jsx
@@ -2,7 +2,7 @@ import { categorieButton, activedCategorie } from './CategorieButton.module.scss
 import setCategorie from './setCategorie';
 
 function CategorieButton({ name, ImageCategorie, id, userCategories, setUserCategories }) {
-    const isActived = userCategories.find(categorie => categorie === id), text = isActived ? ` ${isActived}` : '';
+    const isActived = userCategories.find(categorie => categorie === id), text = isActived ? ` ${activedCategorie}` : '';
 
     return <li className={`${categorieButton}${text}`} id={id} onClick={({ target }) => setCategorie(target, { userCategories, setUserCategories })}>
         <div>
