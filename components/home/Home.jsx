1c8d40b88a4113df40a97923823a0299f13d8617
Danger <104269678+dangerprogrammer@users.noreply.github.com>
2023-09-15 01:55:11 -0300

    .

--- components/home/Home.jsx
+++ components/home/Home.jsx
@@ -1,10 +1,12 @@
 import { homeContainer } from './Home.module.scss';
+import MainContent from './main-content/MainContent';
 import Sidebar from './sidebar/Sidebar';
 
 function Home({ userCategories, setUserCategories, ...login }) {
     
     return <main className={homeContainer}>
         <Sidebar { ...login } userCategories={userCategories} setUserCategories={setUserCategories}/>
+        <MainContent />
     </main>
 };
 
