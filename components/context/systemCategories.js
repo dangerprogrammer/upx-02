c23811b3f7d38475f1917bc3514fd42ed48b13f3
Danger <104269678+dangerprogrammer@users.noreply.github.com>
2023-09-15 02:13:01 -0300

    .

--- components/context/systemCategories.js
+++ components/context/systemCategories.js
@@ -2,10 +2,10 @@ import Image from "next/image";
 import brokenPhone from "@/public/imgs/broken-phone.png";
 
 const systemCategories = [
-    { name: 'Eletrônico estragado', ImageCategorie: () => <Image alt='' src={brokenPhone}/>, id: 'damaged-phones1' },
-    { name: 'Recondicionar', ImageCategorie: () => <Image alt='' src={brokenPhone}/>, id: 'damaged-phones2' },
-    { name: 'Peças e acessórios', ImageCategorie: () => <Image alt='' src={brokenPhone}/>, id: 'damaged-phones3' },
-    { name: 'Outros', ImageCategorie: () => <Image alt='' src={brokenPhone}/>, id: 'damaged-phones4' },
+    { name: 'Eletrônico estragado', ImageCategorie: () => <Image alt='' src={brokenPhone}/>, id: 'damaged-eletronic' },
+    { name: 'Recondicionar', ImageCategorie: () => <Image alt='' src={brokenPhone}/>, id: 'redirect' },
+    { name: 'Peças e acessórios', ImageCategorie: () => <Image alt='' src={brokenPhone}/>, id: 'acessories' },
+    { name: 'Outros', ImageCategorie: () => <Image alt='' src={brokenPhone}/>, id: 'others' },
 ];
 
 export default systemCategories;
---
+++ components/home/main-content/navbar/Navbar.jsx
@@ -0,0 +1,7 @@
+import { navbar } from './Navbar.module.scss';
+
+function Navbar() {
+    return <nav className={navbar}></nav>
+};
+
+export default Navbar;
