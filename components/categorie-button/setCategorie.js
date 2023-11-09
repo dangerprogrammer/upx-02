6d17bff029af4fe33d0b2923f4c71294c11de461
Danger <104269678+dangerprogrammer@users.noreply.github.com>
2023-11-09 15:44:26 -0300

    .

--- components/categorie-button/setCategorie.js
+++ components/categorie-button/setCategorie.js
@@ -1,14 +1,16 @@
 import { activedCategorie } from './CategorieButton.module.scss';
 
-function setCategorie({ id, classList }, { userCategories, setUserCategories }) {
-    const cloneCategories = [...userCategories], hasActived = classList.toggle(activedCategorie);
+function setCategorie({ id, classList }, { setUserCategories }) {
+    setUserCategories(currentCateogires => {
+        const cloneCategories = [...currentCateogires], hasActived = classList.toggle(activedCategorie);
 
-    if (hasActived) {
-        if (cloneCategories.indexOf(id) === -1) cloneCategories.push(id);
-    } else cloneCategories.splice(cloneCategories.indexOf(id), 1);
+        if (hasActived) {
+            if (cloneCategories.indexOf(id) === -1) cloneCategories.push(id);
+        } else cloneCategories.splice(cloneCategories.indexOf(id), 1);
 
-    localStorage.setItem('user-categories', JSON.stringify(cloneCategories));
-    setUserCategories(cloneCategories);
+        localStorage.setItem('user-categories', JSON.stringify(cloneCategories));
+        return cloneCategories;
+    });
 };
 
 export default setCategorie;
