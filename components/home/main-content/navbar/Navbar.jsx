09604b2e057297dc8ee0ed8bc4850f96a5589bdd
Danger <104269678+dangerprogrammer@users.noreply.github.com>
2023-11-15 17:54:51 -0300

    .

--- components/home/main-content/navbar/Navbar.jsx
+++ components/home/main-content/navbar/Navbar.jsx
@@ -1,7 +1,13 @@
-import { navbar } from './Navbar.module.scss';
+import { navbar, searchbarContainer, searchbar, searchIcon } from './Navbar.module.scss';
+import SearchIcon from '@/assets/svgs/search.svg';
 
 function Navbar() {
-    return <nav className={navbar}>Navbar</nav>
+    return <nav className={navbar}>
+        <main className={searchbarContainer}>
+            <SearchIcon className={searchIcon}/>
+            <input type="text" id="searchbar" placeholder='Pesquise por algum produto!' className={searchbar}/>
+        </main>
+    </nav>
 };
 
 export default Navbar;
