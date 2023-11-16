5ea34ef6e3c65472e269df43be48fda4a730b993
Danger <104269678+dangerprogrammer@users.noreply.github.com>
2023-11-16 20:48:47 -0300

    .

--- components/home/main-content/product/Product.jsx
+++ components/home/main-content/product/Product.jsx
@@ -1,7 +1,15 @@
-import { productStyles } from './Product.module.scss';
+import { productStyles, flexRow } from './Product.module.scss';
 
 function Product({categorie, product: {name, desc, price}, user, local}) {
-    return <li className={productStyles}>Produto!</li>
+    return <li className={productStyles}>
+        <span className={flexRow}>
+            <h2>{name}</h2>
+            <strong>{price.toLocaleString('pt-br', { style: 'currency', currency: 'BRL'})}</strong>
+        </span>
+        <h3>{user}</h3>
+        <h4>{local}</h4>
+        <p>{desc}</p>
+    </li>
 };
 
 export default Product;
