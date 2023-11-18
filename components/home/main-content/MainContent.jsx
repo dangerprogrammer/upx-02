911e336b1f8d9fd07a649010afb801c5f11aefe7
Danger <104269678+dangerprogrammer@users.noreply.github.com>
2023-11-18 20:01:49 +0000

    .

--- components/home/main-content/MainContent.jsx
+++ components/home/main-content/MainContent.jsx
@@ -1,21 +1,15 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { mainContent, gridProducts } from './MainContent.module.scss';
 import Footer from './footer/Footer.jsx';
 import Navbar from './navbar/Navbar.jsx';
 import Product from './product/Product';
 
 function MainContent({productsList, userCategories}) {
-    const [searchValue, setSearchValue] = useState(""),
-        filtedCategories = productsList.filter(({categorie}) => userCategories.find(userCat => userCat == categorie));
-    
-    let filtredProducts = filtedCategories;
-
-    useEffect(() => {
-        filtredProducts = filtedCategories.filter(({product: {name}}) => name.startsWith(searchValue));
-    }, [searchValue]);
+    const filtedCategories = productsList.filter(({categorie}) => userCategories.find(userCat => userCat == categorie)),
+        [filtredProducts, setFiltredProducts] = useState(filtedCategories);
 
     return <main className={mainContent}>
-        <Navbar {...{setSearchValue}}/>
+        <Navbar {...{setFiltredProducts, filtedCategories}}/>
         <section className={gridProducts}>
             {filtredProducts.length
             ? filtredProducts.map(({ ...context }, ind) => <Product { ...context } key={ind}/>)
--- components/home/main-content/navbar/Navbar.jsx
+++ components/home/main-content/navbar/Navbar.jsx
@@ -1,11 +1,11 @@
 import { navbar, searchbarContainer, searchbar, searchIcon } from './Navbar.module.scss';
 import SearchIcon from '@/assets/svgs/search.svg';
 
-function Navbar({ setSearchValue }) {
+function Navbar({ setFiltredProducts, filtedCategories }) {
     return <nav className={navbar}>
         <main className={searchbarContainer}>
             <SearchIcon className={searchIcon}/>
-            <input type="text" id="searchbar" placeholder='Pesquise por algum produto!' className={searchbar} onChange={({ target: {value} }) => setSearchValue(value)}/>
+            <input type="text" id="searchbar" placeholder='Pesquise por algum produto!' className={searchbar} onChange={({ target: {value} }) => setFiltredProducts(() => filtedCategories.filter(({product: {name}}) => name.startsWith(value)))}/>
         </main>
     </nav>
 };
