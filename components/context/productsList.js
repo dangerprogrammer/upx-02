61a59ca1709adaff17e928233e08c51a75317e6c
kkjogador <kvictor727@gmail.com>
2023-11-18 13:31:58 -0300

    .

--- components/context/productsList.js
+++ components/context/productsList.js
@@ -11,6 +11,9 @@ const productsList = [
     { categorie: 'damaged-eletronic', product: { name: 'iPhone 11', desc: 'lorem ipsum', price: 250 }, user: 'Patrick', local: 'Sorocaba' },
     { categorie: 'damaged-eletronic', product: { name: 'iPhone 11', desc: 'lorem ipsum', price: 250 }, user: 'Patrick', local: 'Sorocaba' },
     { categorie: 'damaged-eletronic', product: { name: 'iPhone 11', desc: 'lorem ipsum', price: 250 }, user: 'Patrick', local: 'Sorocaba' },
+    { categorie: 'damaged-eletronic', product: { name: 'iPhone 11', desc: 'lorem ipsum', price: 250 }, user: 'Patrick', local: 'Sorocaba' },
+    { categorie: 'damaged-eletronic', product: { name: 'iPhone 11', desc: 'lorem ipsum', price: 250 }, user: 'Patrick', local: 'Sorocaba' },
+    { categorie: 'damaged-eletronic', product: { name: 'iPhone 11', desc: 'lorem ipsum', price: 250 }, user: 'Patrick', local: 'Sorocaba' },
 ];
 
 export default productsList;
