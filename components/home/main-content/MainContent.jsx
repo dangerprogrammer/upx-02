f611ba6aab0139ad43aa00fa6333828e95f8f323
Danger <104269678+dangerprogrammer@users.noreply.github.com>
2023-11-18 20:04:38 +0000

    .

--- components/home/main-content/MainContent.jsx
+++ components/home/main-content/MainContent.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { mainContent, gridProducts } from './MainContent.module.scss';
 import Footer from './footer/Footer.jsx';
 import Navbar from './navbar/Navbar.jsx';
@@ -8,12 +8,14 @@ function MainContent({productsList, userCategories}) {
     const filtedCategories = productsList.filter(({categorie}) => userCategories.find(userCat => userCat == categorie)),
         [filtredProducts, setFiltredProducts] = useState(filtedCategories);
 
+    useEffect(() => setFiltredProducts(filtedCategories), [userCategories]);
+
     return <main className={mainContent}>
         <Navbar {...{setFiltredProducts, filtedCategories}}/>
         <section className={gridProducts}>
             {filtredProducts.length
             ? filtredProducts.map(({ ...context }, ind) => <Product { ...context } key={ind}/>)
-            : <h1>Não há produtos disponíveis na(s) categoria(s) selecionada(s)!</h1>}
+            : <h1>Não há produtos disponíveis!</h1>}
         </section>
         <Footer />
     </main>
