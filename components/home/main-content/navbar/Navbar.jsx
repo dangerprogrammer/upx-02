6576dc3cdf2268b4275c00ca184b937c754f1fff
Danger <104269678+dangerprogrammer@users.noreply.github.com>
2023-11-19 19:04:47 +0000

    .

--- components/home/main-content/navbar/Navbar.jsx
+++ components/home/main-content/navbar/Navbar.jsx
@@ -1,16 +1,19 @@
-import { navbar, searchbarContainer, searchbar, searchIcon } from './Navbar.module.scss';
+import { navbar, searchbarContainer, searchbar, searchContainer, searchIcon, personContainer } from './Navbar.module.scss';
 import SearchIcon from '@/assets/svgs/search.svg';
 
 function Navbar({ setFiltredProducts, filtedCategories }) {
     return <nav className={navbar}>
-        <main className={searchbarContainer}>
-            <SearchIcon className={searchIcon}/>
-            <input type="text" id="searchbar" placeholder='Pesquise por algum produto!' className={searchbar} onChange={({ target: {value} }) =>
-            setFiltredProducts(() =>
-                filtedCategories.filter(({product: {name}}) => name.toLowerCase().includes(value.toLowerCase()))
-            )
-            }/>
+        <main className={searchContainer}>
+            <section className={searchbarContainer}>
+                <SearchIcon className={searchIcon}/>
+                <input type="text" id="searchbar" placeholder='Pesquise por algum produto!' className={searchbar} onChange={({ target: {value} }) =>
+                setFiltredProducts(() =>
+                    filtedCategories.filter(({product: {name}}) => name.toLowerCase().includes(value.toLowerCase()))
+                )
+                }/>
+            </section>
         </main>
+        <button className={personContainer}>Button!</button>
     </nav>
 };
 
