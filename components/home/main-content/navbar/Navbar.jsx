1c0ab9e8cb3e1df071d462ac0bdc61539352a000
Danger <104269678+dangerprogrammer@users.noreply.github.com>
2023-11-19 15:57:13 +0000

    .

--- components/home/main-content/navbar/Navbar.jsx
+++ components/home/main-content/navbar/Navbar.jsx
@@ -5,7 +5,11 @@ function Navbar({ setFiltredProducts, filtedCategories }) {
     return <nav className={navbar}>
         <main className={searchbarContainer}>
             <SearchIcon className={searchIcon}/>
-            <input type="text" id="searchbar" placeholder='Pesquise por algum produto!' className={searchbar} onChange={({ target: {value} }) => setFiltredProducts(() => filtedCategories.filter(({product: {name}}) => name.includes(value)))}/>
+            <input type="text" id="searchbar" placeholder='Pesquise por algum produto!' className={searchbar} onChange={({ target: {value} }) =>
+            setFiltredProducts(() =>
+                filtedCategories.filter(({product: {name}}) => name.toLowerCase().includes(value.toLowerCase()))
+            )
+            }/>
         </main>
     </nav>
 };
