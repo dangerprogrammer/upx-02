c1c5007095d3b929865c6241341453d5402be591
Danger <104269678+dangerprogrammer@users.noreply.github.com>
2023-11-19 14:23:35 +0000

    .

--- components/context/productsList.js
+++ components/context/productsList.js
@@ -1,4 +1,19 @@
-import exampleProduct from "@/public/imgs/example.webp";
+import product1 from "@/public/imgs/product-1.webp";
+import product2 from "@/public/imgs/product-2.webp";
+import product3 from "@/public/imgs/product-3.webp";
+import product4 from "@/public/imgs/product-4.webp";
+import product5 from "@/public/imgs/product-5.webp";
+import product6 from "@/public/imgs/product-6.webp";
+import product7 from "@/public/imgs/product-7.webp";
+import product8 from "@/public/imgs/product-8.webp";
+import product9 from "@/public/imgs/product-9.webp";
+import product10 from "@/public/imgs/product-10.webp";
+import product11 from "@/public/imgs/product-11.webp";
+import product12 from "@/public/imgs/product-12.webp";
+import product13 from "@/public/imgs/product-13.webp";
+import product14 from "@/public/imgs/product-14.webp";
+import product15 from "@/public/imgs/product-15.webp";
+import product16 from "@/public/imgs/product-16.webp";
 
 const productsList = [
     { categorie: 'damaged-eletronic', product: { name: 'iPhone 11', desc: 'lorem ipsum', price: 250, photo: exampleProduct }, user: 'Patrick', local: 'Sorocaba' },
