bf5b9058f7979a4c0e27c3756bf2f9640e24f74b
Danger <104269678+dangerprogrammer@users.noreply.github.com>
2023-11-19 15:48:10 +0000

    .

--- components/home/main-content/product/Product.jsx
+++ components/home/main-content/product/Product.jsx
@@ -1,5 +1,6 @@
-import { productStyles, flexRow, imageStyles } from './Product.module.scss';
+import { productStyles, flexRow, imageStyles, localStyles } from './Product.module.scss';
 import Image from "next/image";
+import Home from '@/assets/svgs/home-outline.svg';
 
 function Product({product: {name, Desc, price, photo}, user, local}) {
     return <li className={productStyles}>
@@ -9,7 +10,7 @@ function Product({product: {name, Desc, price, photo}, user, local}) {
             <h2>{price.toLocaleString('pt-br', { style: 'currency', currency: 'BRL'})}</h2>
         </span>
         <h3>{user}</h3>
-        <h4>{local}</h4>
+        <h4 className={localStyles}><Home/>{local}</h4>
         <p>
             <Desc/>
         </p>
