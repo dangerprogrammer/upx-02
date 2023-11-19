5faf874d1694bf059248d623ceeff6b4315f53ae
Danger <104269678+dangerprogrammer@users.noreply.github.com>
2023-11-19 19:58:17 +0000

    .

--- scripts/logout.js
+++ scripts/logout.js
@@ -1,8 +1,6 @@
 import { redirect } from 'next/navigation';
 
 function logout() {
-    const { redirect } = useRouter();
-
     localStorage.removeItem('user-data');
 
     return redirect('/login');
