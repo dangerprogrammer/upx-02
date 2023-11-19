36c9ca42f7afe14ec6eb789a98c633acc9ca0e8b
Danger <104269678+dangerprogrammer@users.noreply.github.com>
2023-11-19 16:23:07 +0000

    .

--- components/home/main-content/product/Product.jsx
+++ components/home/main-content/product/Product.jsx
@@ -1,10 +1,13 @@
-import { productStyles, flexRow, imageStyles, localStyles, important, wrapLines } from './Product.module.scss';
+import { productStyles, flexRow, imageStyles, localStyles, important, wrapLines, shadow } from './Product.module.scss';
 import Image from "next/image";
 import Home from '@/assets/svgs/home-outline.svg';
 
 function Product({product: {name, Desc, price, photo}, user, local}) {
     return <li className={productStyles}>
-        <Image alt='' src={photo} className={imageStyles}/>
+        <div>
+            <Image alt='' src={photo} className={imageStyles}/>
+            <Image alt='' src={photo} className={`${imageStyles} ${shadow}`}/>
+        </div>
         <span className={flexRow}>
             <h2 className={wrapLines}>{name}</h2>
             <h2 className={important}>{price.toLocaleString('pt-br', { style: 'currency', currency: 'BRL'})}</h2>
@@ -12,7 +15,7 @@ function Product({product: {name, Desc, price, photo}, user, local}) {
         <h3>{user}</h3>
         <h4 className={localStyles}><Home/>{local}</h4>
         <p>
-            <Desc className={wrapLines}/>
+            <Desc className={wrapLines} style={{'--wrap': 3}}/>
         </p>
     </li>
 };
