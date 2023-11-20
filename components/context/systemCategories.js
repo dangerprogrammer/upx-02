a44ea108d80b4b93c9083be687dd7e7f5bc6f8e6
Danger <104269678+dangerprogrammer@users.noreply.github.com>
2023-11-20 21:52:45 +0000

    .

--- components/context/systemCategories.js
+++ components/context/systemCategories.js
@@ -1,11 +1,14 @@
 import Image from "next/image";
-import brokenPhone from "@/public/imgs/broken-phone.png";
+import damagedEletronic from "@/public/imgs/damaged-eletronic.webp";
+import redirect from "@/public/imgs/redirect.webp";
+import acessories from "@/public/imgs/acessories.webp";
+import others from "@/public/imgs/others.webp";
 
 const systemCategories = [
-    { name: 'Eletrônico estragado', ImageCategorie: () => <Image alt='' src={brokenPhone}/>, id: 'damaged-eletronic' },
-    { name: 'Recondicionar', ImageCategorie: () => <Image alt='' src={brokenPhone}/>, id: 'redirect' },
-    { name: 'Peças e acessórios', ImageCategorie: () => <Image alt='' src={brokenPhone}/>, id: 'acessories' },
-    { name: 'Outros', ImageCategorie: () => <Image alt='' src={brokenPhone}/>, id: 'others' },
+    { name: 'Eletrônico estragado', ImageCategorie: () => <Image alt='' src={damagedEletronic}/>, id: 'damaged-eletronic' },
+    { name: 'Recondicionar', ImageCategorie: () => <Image alt='' src={redirect}/>, id: 'redirect' },
+    { name: 'Peças e acessórios', ImageCategorie: () => <Image alt='' src={acessories}/>, id: 'acessories' },
+    { name: 'Outros', ImageCategorie: () => <Image alt='' src={others}/>, id: 'others' },
 ];
 
 export default systemCategories;
