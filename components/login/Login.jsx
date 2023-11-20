e760ec8e30ecfbdaee789257847b2ef7ccd81487
Danger <104269678+dangerprogrammer@users.noreply.github.com>
2023-11-20 21:53:29 +0000

    .

--- components/login/Login.jsx
+++ components/login/Login.jsx
@@ -1,18 +1,12 @@
 import verifyLogin from '@/scripts/verifyLogin';
 import TextInput from '../text-input/TextInput';
 import
-    { mainContainer, container, backgroundContainer, formContent, inputsContainer, loginInput, submitButton, palmStyles }
+    { mainContainer, container, formContent, inputsContainer, loginInput, submitButton }
     from './Login.module.scss';
-// import Image from 'next/image';
-// import palm from '@/public/imgs/palm.png';
-// import recycleWallpaper from '@/public/imgs/recycle-wallpaper.jpeg';
 
 function LoginContainer({ children, setLogin }) {
     return <main className={mainContainer}>
         <section className={container}>
-            <span className={backgroundContainer}>
-                {/* <Image alt='' src={recycleWallpaper}/> */}
-            </span>
             <article className={formContent}>
                 <h1>Faça login com seu nome ou entre com sua conta Google!</h1>
                 <ul className={inputsContainer}>
@@ -20,9 +14,6 @@ function LoginContainer({ children, setLogin }) {
                     <TextInput id='input-email' placeholder='Digite seu e-mail' type='email' className={loginInput}/>
                     <button className={submitButton} onClick={() => verifyLogin(setLogin)}>
                         <span>Fazer Login</span>
-                        <div className={palmStyles}>
-                            {/* <Image alt='' src={palm}/> */}
-                        </div>
                     </button>
                 </ul>
                 { children }
