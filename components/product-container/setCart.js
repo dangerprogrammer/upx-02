04536e4f3327cf44fc3d64df8277a2d1817aaea0
Danger <104269678+dangerprogrammer@users.noreply.github.com>
2023-11-20 23:10:09 +0000

    .

--- components/product-container/setCart.js
+++ components/product-container/setCart.js
@@ -2,8 +2,9 @@ function setCart({ add, product, product: {id} }, { setUserCart }) {
     setUserCart(userCart => {
         const cloneCart = [...userCart], cartIDs = cloneCart.map(({ id }) => id);
 
-        if (add) cloneCart.push(product);
-        else cloneCart.splice(cartIDs.indexOf(id), 1);
+        if (add) {
+            if (cartIDs.indexOf(id) === -1) cloneCart.push(product);
+        } else cloneCart.splice(cartIDs.indexOf(id), 1);
 
         console.log(cloneCart);
         return cloneCart;
