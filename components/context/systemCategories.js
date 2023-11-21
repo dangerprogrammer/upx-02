10c641c3b8f897e80c4dd8afee9788f57f8c785e
Danger <104269678+dangerprogrammer@users.noreply.github.com>
2023-11-21 14:19:05 +0000

    .

--- components/context/systemCategories.js
+++ components/context/systemCategories.js
@@ -1,14 +1,13 @@
-import Image from "next/image";
 import damagedEletronic from "@/public/imgs/damaged-eletronic.webp";
 import redirect from "@/public/imgs/redirect.webp";
 import acessories from "@/public/imgs/acessories.webp";
 import others from "@/public/imgs/others.webp";
 
 const systemCategories = [
-    { name: 'Eletrônico estragado', ImageCategorie: () => <Image alt='' src={damagedEletronic}/>, id: 'damaged-eletronic' },
-    { name: 'Recondicionar', ImageCategorie: () => <Image alt='' src={redirect}/>, id: 'redirect' },
-    { name: 'Peças e acessórios', ImageCategorie: () => <Image alt='' src={acessories}/>, id: 'acessories' },
-    { name: 'Outros', ImageCategorie: () => <Image alt='' src={others}/>, id: 'others' },
+    { name: 'Eletrônico estragado', imageSrc: damagedEletronic, id: 'damaged-eletronic' },
+    { name: 'Recondicionar', imageSrc: redirect, id: 'redirect' },
+    { name: 'Peças e acessórios', imageSrc: acessories, id: 'acessories' },
+    { name: 'Outros', imageSrc: others, id: 'others' },
 ];
 
 export default systemCategories;
--- components/home/sidebar/categorie-button/CategorieButton.jsx
+++ components/home/sidebar/categorie-button/CategorieButton.jsx
@@ -1,13 +1,14 @@
 import { categorieButton, activedCategorie, verifyed } from './CategorieButton.module.scss';
 import setCategorie from './setCategorie';
 import Checkmark from '@/assets/svgs/checkmark-outline.svg';
+import Image from "next/image";
 
-function CategorieButton({ name, ImageCategorie, id, userCategories, setUserCategories }) {
+function CategorieButton({ name, imageSrc, id, userCategories, setUserCategories }) {
     const isActived = userCategories.find(categorie => categorie === id), text = isActived ? ` ${activedCategorie}` : '';
 
     return <li className={`${categorieButton + text}`} id={id} onClick={({ target }) => setCategorie(target, { userCategories, setUserCategories })}>
         <div>
-            <ImageCategorie/>
+            <Image alt='' src={imageSrc}/>
         </div>
         <span>{name}</span>
         <div className={verifyed}>
