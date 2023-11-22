27db99a0064d6f1473acc11545f19a1eb05818ec
Patrick <danger.programmer.js@gmail.com>
2023-11-22 15:49:24 -0300

    .

--- pages/home/Home.jsx
+++ pages/home/Home.jsx
@@ -3,11 +3,11 @@ import { homeContainer } from './Home.module.scss';
 import MainContent from './main-content/MainContent';
 import Sidebar from './sidebar/Sidebar';
 
-function Home({ login: {uniqueID, ...loginContexts}, userProducts, ...contexts }) {
-    return uniqueID ? <main className={homeContainer}>
+function Home({ login: {...loginContexts}, userProducts, ...contexts }) {
+    return <main className={homeContainer}>
         <Sidebar {...{...contexts, ...loginContexts, uniqueID}}/>
         <MainContent { ...contexts }/>
-    </main> : <LoadingContainer/>
+    </main>
 };
 
 export default Home;
