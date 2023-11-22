d158b9053f293e6f90e3a147543657ad7d315be1
Patrick <danger.programmer.js@gmail.com>
2023-11-22 16:13:05 -0300

    .

--- pages/home/Home.jsx
+++ pages/home/Home.jsx
@@ -3,8 +3,9 @@ import { homeContainer } from './Home.module.scss';
 import MainContent from './main-content/MainContent';
 import Sidebar from './sidebar/Sidebar';
 
-function Home({ userProducts, ...contexts }) {
-    console.log(contexts);
+function Home({ login, userProducts, ...contexts }) {
+    console.log(login);
+    return <div>Opa!</div>
     return <main className={homeContainer}>
         <Sidebar {...{...contexts}}/>
         <MainContent { ...contexts }/>
