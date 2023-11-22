a8156ca4d0491f326d35781ad9e4cf926ae9e7d7
Patrick <danger.programmer.js@gmail.com>
2023-11-22 16:32:49 -0300

    .

--- pages/home/sidebar/categorie-button/CategorieButton.jsx
+++ pages/home/sidebar/categorie-button/CategorieButton.jsx
@@ -5,6 +5,7 @@ import Checkmark from '@/assets/svgs/checkmark-outline.svg';
 import Image from "next/image";
 
 function CategorieButton({ name, imageSrc, id, userCategories, setUserCategories, activeButton, changeButton }) {
+    return;
     const [isActived, setActived] = useState(userCategories.find(categorie => categorie === id) || activeButton),
         text = (isActived) ? ` ${activedCategorie}` : '';
 
