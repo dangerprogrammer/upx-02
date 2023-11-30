7a200f23292a123d27e8e470a626e03e9cc7f043
Danger <104269678+dangerprogrammer@users.noreply.github.com>
2023-11-29 23:08:20 -0300

    .

--- components/product/Product.jsx
+++ components/product/Product.jsx
@@ -1,26 +1,29 @@
 import { cart, productStyles, imageStyles, localStyles, wrapLines, shadow, showHover, categorieName, greenColor, removeCartStyles, restoreCart } from './Product.module.scss';
-import Image from "next/image";
 import Home from '@/assets/svgs/home-outline.svg';
 import Cart from '@/assets/svgs/cart-outline.svg';
 import Trash from '@/assets/svgs/trash-outline.svg';
 import { useRouter } from 'next/navigation';
 import realCash from '@/scripts/realCash';
 import { useState } from 'react';
+import Example from "@/public/imgs/example.webp";
 
-function Product({systemCategories, categorie, product: {name, Desc, price, photo, id}, local, removeCart}) {
+function Product({systemCategories, categorie, product: {name, Desc, price = 10, photo, id}, local, finishedProduct = !0, removeCart}) {
     const { push } = useRouter(), productCategorie = systemCategories.find(({ id }) => id == categorie),
         [inCart, setInCart] = useState(!0);
 
     return <li className={productStyles}>
-        <div onClick={() => push(`/${id}`)}>
-            <Image alt='' src={photo} className={imageStyles} priority/>
-            <Image alt='' src={photo} className={`${imageStyles} ${shadow}`}/>
+        {finishedProduct ? <div onClick={() => push(`/${id}`)}>
+            <img alt='' src={photo || Example.src} className={imageStyles} priority/>
+            <img alt='' src={photo || Example.src} className={`${imageStyles} ${shadow}`}/>
             <span className={showHover}>Ver produto!</span>
-        </div>
+        </div> : <div>
+            <img alt='' src={photo || Example.src} className={imageStyles} priority/>
+            <img alt='' src={photo || Example.src} className={`${imageStyles} ${shadow}`}/>
+        </div>}
         <div className={categorieName}>
             <span>{productCategorie.name}</span>
         </div>
-        <h3 className={wrapLines}>{name}</h3>
+        <h3 className={wrapLines}>{name || 'Product Name'}</h3>
         <h2 className={greenColor}>{realCash(price)}</h2>
         <h4 className={localStyles}><Home/>{local}</h4>
         <p>
--- pages/new-product/NewProduct.jsx
+++ pages/new-product/NewProduct.jsx
@@ -1,46 +1,60 @@
 import BackPage from '@/components/back-page/BackPage';
 import LoadingContainer from '../../components/loading-container/LoadingContainer';
 import { newProduct, productContent } from './NewProduct.module.scss';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import Product from '@/components/product/Product';
+import realCash from '@/scripts/realCash';
 
 function NewProduct({ login: {uniqueID, given_name, name}, systemCategories, userProducts, setUserProducts, productsList }) {
-    console.clear();
-    const productObject = {
-        categorie: systemCategories[0].id,
+    const [productPhoto, setProductPhoto] = useState(null),
+        [categorie, setCategorie] = useState(systemCategories[0].id),
+        [productName, setProductName] = useState(''),
+        [productDesc, setProductDesc] = useState(),
+        [productPrice, setProductPrice] = useState(0),
+        productObject = {
+        categorie,
         product: {
-            id: `product-${productsList.length + userProducts.length}`
+            name: productName,
+            Desc:  ({ ...contexts }) => <span { ...contexts }>{productDesc || 'Description'}</span>,
+            price: productPrice, photo: productPhoto, id: `product-${productsList.length + userProducts.length}`
         },
+        user: name, local: 'Sorocaba, São Paulo (SP)'
     };
 
-    console.log(productsList);
-
-    useEffect(() => {
-        console.log(productObject);
-    }, [productObject.categorie, productObject.product.name, productObject.product.Desc]);
-
     return uniqueID ? <main className={newProduct}>
-        <h1>Crie seu próprio produto {given_name}!</h1>
+        <h1>Crie seu próprio produto, {given_name}!</h1>
         <section className={productContent}>
-            <h1>Categoria do produto</h1>
-            <select>
+            <h1>Categoria do Produto</h1>
+            <select onChange={({ target: {selectedIndex, options} }) => setCategorie(options[selectedIndex].value)}>
                 {systemCategories.map(({ name, id }, ind) => <option key={ind} value={id}>{name}</option>)}
             </select>
         </section>
         <section className={productContent}>
             <h1>Nome do Produto</h1>
-            <input type="text" onChange={({ target: {value} }) => (productObject.product.name = value, console.log(productObject))}/>
+            <input type="text" onChange={({ target: {value} }) => setProductName(value)}/>
         </section>
         <section className={productContent}>
             <h1>Descrição</h1>
-            <textarea cols="30" rows="3" onChange={({ target: {value} }) => (productObject.product.Desc = ({ ...contexts }) => <span { ...contexts }>
-                {value}
-            </span>)}></textarea>
+            <textarea cols="30" rows="3" onChange={({ target: {value} }) => (setProductDesc(<pre>{value || 'Description'}</pre>))}></textarea>
         </section>
         <section className={productContent}>
             <h1>Preço</h1>
-            <input type="text" onChange={({ target: {value} }) => (productObject.product.price = +value)}/>
+            <span>
+                <input type="number" min={0} onChange={({ target: {value} }) => setProductPrice(Math.max(+value, 0))}/>
+                <p>{realCash(productPrice)}</p>
+            </span>
         </section>
-        <p>Descrição do produto, preço, foto</p>
+        <section className={productContent}>
+            <h1>Foto do produto</h1>
+            <input type="file" id="product-photo" onChange={({ target: {files} }) => files[0] && setProductPhoto(URL.createObjectURL(files[0]))}/>
+            <label htmlFor="product-photo">
+                {productPhoto ? <img src={productPhoto} alt=""/> : <span>Escolha a foto do produto!</span>}
+            </label>
+        </section>
+        <span>
+            <p>Prévia</p>
+            <Product {...{...productObject, systemCategories, finishedProduct: !1}}/>
+        </span>
         <BackPage/>
     </main> : <LoadingContainer/>
 };
